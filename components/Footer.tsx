f3a16d5acb2e7aba2afca7ab4c68c31740e9933f
chounic <hakim.chouioukh@gmail.com>
2022-03-16 00:57:59 +0100

    first

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div style={{ backgroundColor: "blue", fontSize: 50}}>
+            Footeeeeeeeeeeeeeeeeeeeeeeer
+        </div>
+    );
+};
+
+export default Footer;
---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div style={{ backgroundColor: "red", fontSize: 50}}>
+            Navbarrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+        </div>
+    );
+};
+
+export default Navbar;
