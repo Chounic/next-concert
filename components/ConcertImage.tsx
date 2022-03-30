31c8322f3244f509f245d8d474a05b59331642a6
Chounic <hakim.chouioukh@gmail.com>
2022-03-30 02:12:36 +0200

    update

--- components/ConcertImage.tsx
+++ components/ConcertImage.tsx
@@ -9,31 +9,31 @@ console.log("🚀 ~ file: concertImage.tsx ~ line 5 ~ ConcertImage ~ data.entiti
   const [artistImageUrl, setArtistImageUrl] = useState('');
 
 
-    useEffect (() => {
-      console.log('image oui oui');
-      ( async () => {
-          if (data.entities[0]) {
-          const res = await fetch(`https://api.spotify.com/v1/search?type=artist&q=${data.title ?? ''}`, {
-              headers: {
-                Authorization: `Bearer ${token}`, 
-              }
-            });
-          console.log("🚀 ~ file: ConcertImage.tsx ~ line 17 ~ res", res);
-          const response = await res.json();
-          if (response.artists.items[0] && response.artists.items[0].images[0]) {
+    // useEffect (() => {
+    //   console.log('image oui oui');
+    //   ( async () => {
+    //       if (data.entities[0]) {
+    //       const res = await fetch(`https://api.spotify.com/v1/search?type=artist&q=${data.title ?? ''}`, {
+    //           headers: {
+    //             Authorization: `Bearer ${token}`, 
+    //           }
+    //         });
+    //       console.log("🚀 ~ file: ConcertImage.tsx ~ line 17 ~ res", res);
+    //       const response = await res.json();
+    //       if (response.artists.items[0] && response.artists.items[0].images[0]) {
 
-            console.log("🚀 ~ file: ConcertImage.tsx ~ line 20 ~ response", response.artists.items[0].images[0].url)
-            setArtistImageUrl(response.artists.items[0].images[0].url);
-          }
-          // console.log("🚀 ~ file: concertImage.tsx ~ line 16 ~ res", res.body)
-          }
-        })();
-    }, [data.entities, token])
+    //         console.log("🚀 ~ file: ConcertImage.tsx ~ line 20 ~ response", response.artists.items[0].images[0].url)
+    //         setArtistImageUrl(response.artists.items[0].images[0].url);
+    //       }
+    //       // console.log("🚀 ~ file: concertImage.tsx ~ line 16 ~ res", res.body)
+    //       }
+    //     })();
+    // }, [data.entities, token])
     
 
     return (
-        <div className='w-50 h-40 bg-rose-600' >
-            {artistImageUrl && <Image src={artistImageUrl} alt="artist photo" width={200} height={200} />}
+        <div className=' h-2/3 relative' >
+            {data.images[0].url && <Image src={data.images[0].url} alt="artist photo" layout='fill' objectFit='inherit' />}
         </div>
     );
 };
