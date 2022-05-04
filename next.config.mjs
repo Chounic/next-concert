469e1624a82dfecc4dd22f87b9b22a4fe242ddcc
Chounic <hakim.chouioukh@gmail.com>
2022-05-05 01:33:22 +0200

    fix type errors

--- next.config.mjs
+++ next.config.mjs
@@ -13,13 +13,6 @@ const nextConfig = {
 
     return config
   }, 
-  typescript: {
-
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-
-    ignoreBuildErrors: true,
-  },
 }
 
 export default nextConfig
