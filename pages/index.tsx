18e9d234d929b8885cc2475848aaa4cbe09d6718
Chounic <hakim.chouioukh@gmail.com>
2022-05-09 00:03:39 +0200

    fix type errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -172,6 +172,7 @@ export async function getStaticProps() {
 
   const attractionsRes = await fetch(`https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=${process.env.ACCESS_TOKEN}&classificationName=[Music]&size=40`);
   const attractionsJsonRes = await attractionsRes.json();
+  console.log("🚀 ~ file: index.tsx ~ line 176 ~ getStaticProps ~ attractionsJsonRes._links", attractionsJsonRes._links)
   const attractions = attractionsJsonRes._embedded.attractions;
   const citiesId = [
     {
